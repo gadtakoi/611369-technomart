4f5ab8901f06606a54a46ab68b25501a0624c4b5
gadtakoi <gadtakoi@gmail.com>
2018-09-07 12:38:25 +0300

    #Оформление контента - 1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,54 @@
 document.addEventListener('DOMContentLoaded', function () {
+  try {
+    var feedback = document.querySelector('.feedback');
+    var writeus = document.querySelector('.modal-write-us');
+    var close = writeus.querySelector('.modal-close');
+    feedback.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      writeus.classList.add('modal-show');
+    });
+    close.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      writeus.classList.remove('modal-show');
+    });
+  } catch (e) {
 
-  var feedback = document.querySelector('.feedback');
-  var writeus = document.querySelector('.modal-write-us');
-  var close = writeus.querySelector('.modal-close');
-  feedback.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    writeus.classList.add('modal-show');
-  });
-  close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    writeus.classList.remove('modal-show');
-  });
+  }
+  try {
+
+    var footermap = document.querySelector('.footer-map');
+    var map = document.querySelector('.modal-map');
+    var mapclose = map.querySelector('.about .modal-close');
+
+    footermap.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      map.classList.add('modal-show');
+    });
+    mapclose.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      map.classList.remove('modal-show');
+    });
+
+  } catch (e) {
+
+  }
+  try {
+
+    var buy = document.querySelector('.buy');
+    var productadd = document.querySelector('.modal-product-add');
+    var productclose = productadd.querySelector('.modal-product-add .modal-close');
+
+    buy.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      productadd.classList.add('modal-show');
+    });
+    productclose.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      productadd.classList.remove('modal-show');
+    });
+
+  } catch (e) {
+
+  }
+
 })
